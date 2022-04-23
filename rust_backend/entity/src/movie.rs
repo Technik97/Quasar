fe20fe979a9e73c288ce77ec36ee2d9ec7ae410e
TechNik <aniket.97.mane@gmail.com>
2022-04-23 20:08:04 +0530

    Added movie model

---
+++ rust_backend/entity/src/movie.rs
@@ -0,0 +1,20 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Deserialize, Serialize)]
+#[sea_orm(table_name = "movies")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    #[serde(skip_deserializing)]
+    pub id: i32,
+
+    #[sea_orm(column_type="Text")]
+    pub title: String,
+
+    pub runtime: String,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
