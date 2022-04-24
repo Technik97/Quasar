3add5ad9083bdff4a7e181706898bac85a6feb90
TechNik <aniket.97.mane@gmail.com>
2022-04-24 12:54:27 +0530

    Added actix setup and ran migrations

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    App, HttpRequest, HttpResponse, HttpServer, Result, Responder, middleware, web
+};
+use entity::movie;
+use entity::movie::Entity as Movie;
+use entity::sea_orm;
+
+use listenfd::ListenFd;
+use std::env;
+use migration::{Migrator, MigratorTrait};
+use sea_orm::DatabaseConnection;
+
+#[derive(Debug, Clone)]
+struct AppState {
+    conn: DatabaseConnection,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    tracing_subscriber::fmt::init();
+
+    dotenv::dotenv().ok();
+    let mut listenfd = ListenFd::from_env();
+    
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let host = env::var("HOST").expect("HOST is not set in .env file");
+    let port = env::var("PORT").expect("PORT is not set in .env file");
+    let server_url = format!("{}:{}", host, port);
+
+    let conn = sea_orm::Database::connect(&db_url).await.unwrap();
+    Migrator::up(&conn, None).await.unwrap();
+
+    let state = AppState { conn };
+
+    let mut server = HttpServer::new(move || {
+        App::new()
+            .data(state.clone())
+            .wrap(middleware::Logger::default())
+            .configure(init)
+    });
+
+    server = match listenfd.take_tcp_listener(0)? {
+        Some(listener) => server.listen(listener)?,
+        None =>  server.bind(&server_url)?,
+    };
+
+    println!("Starting server at {}", server_url);
+    server.run().await?;
+
+    Ok(())
+}
+pub fn init(cfg: &mut web::ServiceConfig) {
+
 }
