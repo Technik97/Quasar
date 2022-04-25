05ee477834e184c76d7e724be595677ebc715f63
TechNik <aniket.97.mane@gmail.com>
2022-04-25 21:29:10 +0530

    Added purgecss config to reduce the tailwind bundle size

--- svelte_frontend/postcss.config.js
+++ svelte_frontend/postcss.config.js
@@ -1,6 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    cssnano({
+      preset: 'default'
+    }),
+    purgecss({
+      content: ['./layouts/**/*.html', './src/**/*.svelte'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+    })
+  ]
 }
