a4ccaec5655feaf27889b2c59b551852e476506e
TechNik <aniket.97.mane@gmail.com>
2022-04-26 22:51:04 +0530

    created model for production

---
+++ rust_backend/entity/src/production.rs
@@ -0,0 +1,19 @@
+use sea_orm::entity::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Deserialize, Serialize)]
+#[sea_orm(table_name = "productions")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    #[serde(skip_deserializing)]
+    pub id: i32,
+
+    #[sea_orm(column_type="Text", unique)]
+    pub name: String,
+
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
