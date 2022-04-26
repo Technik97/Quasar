c70f03b757fd1a76efbefcabc949b519747fc17c
TechNik <aniket.97.mane@gmail.com>
2022-04-26 20:09:41 +0530

    Testing tailwindcss

--- svelte_frontend/src/components/MovieCard.svelte
+++ svelte_frontend/src/components/MovieCard.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
     export let movie;
+    import  '../../public/index.css'
 </script>
 
-<div>
+<div class="text-gray-500 text-center text-lg">
     <h1>{movie.id}</h1>
     <h1>{movie.title}</h1>
     <h1>{movie.runtime}</h1>
