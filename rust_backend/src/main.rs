d520eef69fa99b30f4dca66e9b43c77836dac4c8
TechNik <aniket.97.mane@gmail.com>
2022-05-03 01:17:39 +0530

    Refactored main.rs

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,5 +1,5 @@
 use actix_web::{
-    App, HttpRequest, HttpResponse, HttpServer, Result, Responder, middleware, web, get
+    App, HttpResponse, HttpServer, middleware, web, get
 };
 use actix_cors::Cors;
 use entity::{movie, production ,sea_orm::EntityTrait};
@@ -8,23 +8,23 @@ use entity::production::Entity as Production;
 use entity::sea_orm;
 use serde_json::to_string;
 use listenfd::ListenFd;
-use serde::Serialize;
-use std::env;
 use migration::{Migrator, MigratorTrait};
 use sea_orm::DatabaseConnection;
 
+mod data;
+use crate::data::db;
+
 #[derive(Debug, Clone)]
 struct AppState {
-    conn: DatabaseConnection,
+    conn: DatabaseConnection
 }
-
 
 #[get("/")]
 async fn index(
     data: web::Data<AppState>
 ) -> HttpResponse {
     let conn = &data.conn;
-
+    //(movie::Model, Option<production::Model>)
     let movies: Vec<(movie::Model, Option<production::Model>)> = Movie::find()
                                         .find_also_related(Production)
                                         .all(conn)
@@ -39,8 +39,6 @@ async fn index(
     
 }
 
-
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
@@ -49,14 +47,8 @@ async fn main() -> std::io::Result<()> {
     dotenv::dotenv().ok();
     let mut listenfd = ListenFd::from_env();
     
-    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
-    let host = env::var("HOST").expect("HOST is not set in .env file");
-    let port = env::var("PORT").expect("PORT is not set in .env file");
-    let server_url = format!("{}:{}", host, port);
-
-    // let client_url = env::var("CLIENT_URL").expect("CLIENT_URL is not set in .env file");
-
-    let conn = sea_orm::Database::connect(&db_url).await.unwrap();
+    let conn = db::get_conn().await.to_owned();
+    let server_url = db::get_server_url();
     Migrator::up(&conn, None).await.unwrap();
 
     let state = AppState { conn };
@@ -81,6 +73,7 @@ async fn main() -> std::io::Result<()> {
 
     Ok(())
 }
+
 pub fn init(cfg: &mut web::ServiceConfig) {
     cfg.service(index);
 }
